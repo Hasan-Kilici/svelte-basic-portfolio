0714ed1ba35d82ad7303dd1950fdccb43153ce05
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-25 18:02:46 +0300

    Create About.svelte

---
+++ src/lib/About.svelte
@@ -0,0 +1,26 @@
+<script>
+let img = {
+  src: "https://avatars.githubusercontent.com/u/105741983?v=4",
+  title: "Kawethranın Profil fotoğrafı",
+  class: "rounded w-100",
+  style: "margin-top:1.3vh;margin-bottom:1.3vh"
+}
+  let text = {
+    style: "margin-top:1.3vh;margin-bottom:1.3vh;"
+  }
+</script>
+<div class="container-fluid bg-dark text-light">
+<div class="container">
+ <div class="row">
+   <div class="col-md-3">
+   <img {...img}>
+   </div>
+   <div class="col-md-9">
+   <label {...text}>
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+   </label>
+   </div>
+ </div>
+</div>
+</div>
