64ba3074e4cb65cb110c16fe11225a5f310c7c27
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-25 18:06:15 +0300

    Create Projects.svelte

---
+++ src/lib/Projects.svelte
@@ -0,0 +1,42 @@
+<script>
+let img = {
+  src: "https://avatars.githubusercontent.com/u/105741983?v=4",
+  title: "Kawethranın Profil fotoğrafı",
+  class: "rounded w-100",
+  style: "margin-top:1.3vh;margin-bottom:1.3vh"
+}
+  let text = {
+    style: "margin-top:1.3vh;margin-bottom:1.3vh;"
+  }
+</script>
+<div class="container-fluid bg-dark text-light">
+<div class="container">
+ <div class="row">
+   <div class="col-md-3">
+   <img {...img}>
+   </div>
+   <div class="col-md-9">
+   <label {...text}>
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+   </label>
+   </div>
+ </div>
+</div>
+</div>
+<div class="container">
+<br><h4>My Projects</h4>
+<hr>
+        <div class="col-md-3 d-flex align-items-stretch m-t">
+        <div class="card">
+          <img class="card-img-top" src="https://avatars.githubusercontent.com/u/105741983?v=4">
+          <div class="card-body">
+            <h5>Proje Başlık</h5>
+            <span>Proje açıklama</span><br>
+          </div>
+            <div class="card-footer">
+              <a href=""><button class="btn btn-dark">View Project</button></a>
+              </div>
+        </div>
+      </div>
+</div>
